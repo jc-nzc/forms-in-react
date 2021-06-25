24070df4f6fd819b8c7f3cb608ce7b19735c0f9b
Zero <node.zerocool@gmail.com>
2021-06-24 23:38:52 -0500

    Updates

---
+++ precache-manifest.f7020a086d7281de97ea0369264a2558.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c86b65e16645319ecc530e7bf396e11",
+    "url": "/forms-in-react/index.html"
+  },
+  {
+    "revision": "4ba66c9eaaba22520f95",
+    "url": "/forms-in-react/static/css/main.547ea782.chunk.css"
+  },
+  {
+    "revision": "21e0455ab157e3c726f6",
+    "url": "/forms-in-react/static/js/2.4282bdd0.chunk.js"
+  },
+  {
+    "revision": "4ba66c9eaaba22520f95",
+    "url": "/forms-in-react/static/js/main.c18ed066.chunk.js"
+  },
+  {
+    "revision": "8317b26a6b09497687e0",
+    "url": "/forms-in-react/static/js/runtime~main.15236847.js"
+  }
+]);
